20dab522b8c7afd587a9ee5a6e9ae6dda7e746b8
Diego Santos <diegofmsantos0@gmail.com>
2024-05-30 18:52:50 -0300

    ajuste mensagem, falta total e cliente

--- src/lib/generate-message.ts
+++ src/lib/generate-message.ts
@@ -2,12 +2,14 @@ import { useCartStore } from "@/stores/cart-store"
 import { useCheckoutStore } from "@/stores/checkout-store"
 
 export const generateMessage = () => {
-    const { nome, cnpj, endereco, email, telefone, pagamento, frete, } = useCheckoutStore(state => state)
+    const { nome, cnpj, endereco, email, telefone, pagamento, frete } = useCheckoutStore(state => state)
     const { cart } = useCartStore(state => state)
 
     let pedidos = []
+    let valorTotal = 0
     for (let item of cart) {
-        pedidos.push(`${item.quantity} x ${item.product.linha} (${item.product.materias[item.selectedMaterialIndex]}): ${item.quantity * item.price}`)
+        valorTotal = (item.quantity * item.price)
+        pedidos.push(`${item.quantity} x ${item.product.linha} (${item.product.materias[item.selectedMaterialIndex]}): R$ ${valorTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`)
     }
 
     return `**Dados do Cliente:**
