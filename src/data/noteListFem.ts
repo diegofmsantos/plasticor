2fa9c043f2a2626628c10bb6864c6a4c8bf37926
Diego Santos <diegofmsantos0@gmail.com>
2024-06-03 13:00:29 -0300

    alterando dados da API

--- src/data/noteListFem.ts
+++ src/data/noteListFem.ts
@@ -5,7 +5,7 @@ export const NoteListFem: Notebook[] = [
         id: 1,
         codigos: [4114, 4110, 4111, 4112, 4113],
         linha: 'ASPECTS',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['ASPECTS-1.png', 'ASPECTS-2.png', 'ASPECTS-3.png', 'ASPECTS-4.png', 'ASPECTS-5.png']
     },
@@ -13,7 +13,7 @@ export const NoteListFem: Notebook[] = [
         id: 2,
         codigos: [4149, 4145, 4146, 4147, 4148],
         linha: 'CAPRICHE',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['CAPRICHE-1.png', 'CAPRICHE-2.png', 'CAPRICHE-3.png', 'CAPRICHE-4.png', 'CAPRICHE-5.png']
     },
@@ -21,7 +21,7 @@ export const NoteListFem: Notebook[] = [
         id: 3,
         codigos: [4164, 4160, 4161, 4162, 4163],
         linha: 'CHARM',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['CHARM-1.png', 'CHARM-2.png', 'CHARM-3.png', 'CHARM-4.png', 'CHARM-5.png']
     },
@@ -37,7 +37,7 @@ export const NoteListFem: Notebook[] = [
         id: 5,
         codigos: [4119, 4115, 4116, 4117, 4118],
         linha: 'COSMUS',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['COSMUS-1.png', 'COSMUS-2.png', 'COSMUS-3.png', 'COSMUS-4.png', 'COSMUS-5.png']
     },
@@ -45,7 +45,7 @@ export const NoteListFem: Notebook[] = [
         id: 6,
         codigos: [4129, 4125, 4126, 4127, 4128],
         linha: 'CUTE',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['CUTE-1.png', 'CUTE-2.png', 'CUTE-3.png', 'CUTE-4.png', 'CUTE-5.png']
     },
@@ -53,7 +53,7 @@ export const NoteListFem: Notebook[] = [
         id: 7,
         codigos: [4109, 4105, 4106, 4107, 4108],
         linha: 'FLYS',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['FLYS-1.png', 'FLYS-2.png', 'FLYS-3.png', 'FLYS-4.png', 'FLYS-5.png']
     },
@@ -61,7 +61,7 @@ export const NoteListFem: Notebook[] = [
         id: 8,
         codigos: [4159, 4155, 4156, 4157, 4157],
         linha: 'LOVE',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['LOVE-1.png', 'LOVE-2.png', 'LOVE-3.png', 'LOVE-4.png', 'LOVE-5.png']
     },
@@ -69,7 +69,7 @@ export const NoteListFem: Notebook[] = [
         id: 9,
         codigos: [4104, 4100, 4101, 4102, 4103],
         linha: 'MOMENTS',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['MOMENTS-1.png', 'MOMENTS-2.png', 'MOMENTS-3.png', 'MOMENTS-4.png', 'MOMENTS-5.png']
     },
@@ -77,7 +77,7 @@ export const NoteListFem: Notebook[] = [
         id: 10,
         codigos: [4139, 4135, 4136, 4137, 4138],
         linha: 'PETS',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['PETS-1.png', 'PETS-2.png', 'PETS-3.png', 'PETS-4.png', 'PETS-5.png']
     },
@@ -85,7 +85,7 @@ export const NoteListFem: Notebook[] = [
         id: 11,
         codigos: [4144, 4140, 4141, 4142, 4143],
         linha: 'TROPICALIENTE',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['TROPICALIENTE-1.png', 'TROPICALIENTE-2.png', 'TROPICALIENTE-3.png', 'TROPICALIENTE-4.png', 'TROPICALIENTE-5.png']
     },
@@ -93,7 +93,7 @@ export const NoteListFem: Notebook[] = [
         id: 12,
         codigos: [4154, 4150, 4151, 4152, 4153],
         linha: 'UNICORN-WORLD',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['UNICORN-WORLD-1.png', 'UNICORN-WORLD-2.png', 'UNICORN-WORLD-3.png', 'UNICORN-WORLD-4.png', 'UNICORN-WORLD-5.png']
     },
@@ -101,7 +101,7 @@ export const NoteListFem: Notebook[] = [
         id: 13,
         codigos: [4124, 4120, 4121, 4122, 4123],
         linha: 'WEIRD',
-        materias: ['1x1', '10x1', '12x1', '15x1', '20x1'],
+        materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
         valores: [682.20, 648.00, 586.56, 695.76, 665.28],
         url: ['WEIRD-1.png', 'WEIRD-2.png', 'WEIRD-3.png', 'WEIRD-4.png', 'WEIRD-5.png']
     }
--- src/data/noteListRdzd.ts
+++ src/data/noteListRdzd.ts
@@ -7,7 +7,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'LOVE - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['LOVE-1.png', 'LOVE-2.png', 'LOVE-3.png', 'LOVE-4.png', 'LOVE-5.png']
     },
@@ -17,7 +17,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'CAPRICHE - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['CAPRICHE-1.png', 'CAPRICHE-2.png', 'CAPRICHE-3.png', 'CAPRICHE-4.png', 'CAPRICHE-5.png']
     },
@@ -27,7 +27,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'TROPICALIENTE - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['TROPICALIENTE-1.png', 'TROPICALIENTE-2.png', 'TROPICALIENTE-3.png', 'TROPICALIENTE-4.png', 'TROPICALIENTE-5.png',]
     },
@@ -37,7 +37,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'PETS - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['PETS-1.png', 'PETS-2.png', 'PETS-3.png', 'PETS-4.png', 'PETS-5.png',]
     },
@@ -47,7 +47,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'UNICORN-WORLD - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['UNICORN-WORLD-1.png', 'UNICORN-WORLD-2.png', 'UNICORN-WORLD-3.png', 'UNICORN-WORLD-4.png', 'UNICORN-WORLD-5.png']
     },
@@ -57,7 +57,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'CHARM - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['CHARM-1.png', 'CHARM-2.png', 'CHARM-3.png', 'CHARM-4.png', 'CHARM-5.png',]
     },
@@ -67,7 +67,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'FLYS - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['FLYS-1.png', 'FLYS-2.png', 'FLYS-3.png', 'FLYS-4.png', 'FLYS-5.png',]
     },
@@ -77,7 +77,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'WEIRD - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['WEIRD-1.png', 'WEIRD-2.png', 'WEIRD-3.png', 'WEIRD-4.png', 'WEIRD-5.png',]
     },
@@ -87,7 +87,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'COLOUR - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['COLOUR-1.png', 'COLOUR-2.png', 'COLOUR-3.png', 'COLOUR-4.png', 'COLOUR-5.png',]
     },
@@ -97,7 +97,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'MOMENTS - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['MOMENTS-1.png', 'MOMENTS-2.png', 'MOMENTS-3.png', 'MOMENTS-4.png', 'MOMENTS-5.png',]
     },
@@ -107,7 +107,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'ASPECTS - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['ASPECTS-1.png', 'ASPECTS-2.png', 'ASPECTS-3.png', 'ASPECTS-4.png', 'ASPECTS-5.png',]
     },
@@ -117,7 +117,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'CUTE - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['CUTE-1.png', 'CUTE-2.png', 'CUTE-3.png', 'CUTE-4.png', 'CUTE-5.png',]
     },
@@ -127,7 +127,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'feminino',
         linha: 'COSMUS - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [452.70, 508.56, 470.16],
         url: ['COSMUS-1.png', 'COSMUS-2.png', 'COSMUS-3.png', 'COSMUS-4.png', 'COSMUS-5.png',]
     },
@@ -137,7 +137,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'VELOZES E TUNADOS - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['VELOZES-E-TUNADOS-1.png', 'VELOZES-E-TUNADOS-2.png', 'VELOZES-E-TUNADOS-3.png', 'VELOZES-E-TUNADOS-4.png', 'VELOZES-E-TUNADOS-5.png']
     },
@@ -147,7 +147,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'MOTORCYCLES - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['MOTORCYCLES-1.png', 'MOTORCYCLES-2.png', 'MOTORCYCLES-3.png', 'MOTORCYCLES-4.png', 'MOTORCYCLES-5.png']
     },
@@ -157,7 +157,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'SKULL - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['SKULL-1.png', 'SKULL-2.png', 'SKULL-3.png', 'SKULL-4.png', 'SKULL-5.png']
     },
@@ -167,7 +167,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'EPIC-GAMES - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['EPIC-GAMES-1.png', 'EPIC-GAMES-2.png', 'EPIC-GAMES-3.png', 'EPIC-GAMES-4.png', 'EPIC-GAMES-5.png']
     },
@@ -177,7 +177,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'SPORT-MIX - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['SPORT-MIX-1.png', 'SPORT-MIX-2.png', 'SPORT-MIX-3.png', 'SPORT-MIX-4.png', 'SPORT-MIX-5.png']
     },
@@ -187,7 +187,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'CODE - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['CODE-1.png', 'CODE-2.png', 'CODE-3.png', 'CODE-4.png', 'CODE-5.png']
     },
@@ -197,7 +197,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'HIGH - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['HIGH-1.png', 'HIGH-2.png', 'HIGH-3.png', 'HIGH-4.png', 'HIGH-5.png']
     },
@@ -207,7 +207,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'GRAFITTI - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['GRAFITTI-1.png', 'GRAFITTI-2.png', 'GRAFITTI-3.png', 'GRAFITTI-4.png', 'GRAFITTI-5.png']
     },
@@ -217,7 +217,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'DAIJOBU - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['DAIJOBU-1.png', 'DAIJOBU-2.png', 'DAIJOBU-3.png', 'DAIJOBU-4.png', 'DAIJOBU-5.png']
     },
@@ -227,7 +227,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'KINGDOM - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['KINGDOM-1.png', 'KINGDOM-2.png', 'KINGDOM-3.png', 'KINGDOM-4.png', 'KINGDOM-5.png']
     },
@@ -237,7 +237,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'FIGHT-CLUB - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['FIGHT-CLUB-1.png', 'FIGHT-CLUB-2.png', 'FIGHT-CLUB-3.png', 'FIGHT-CLUB-4.png', 'FIGHT-CLUB-5.png']
     },
@@ -247,7 +247,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'GOSPEL - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['GOSPEL-1.png', 'GOSPEL-2.png', 'GOSPEL-3.png', 'GOSPEL-4.png', 'GOSPEL-4.png', 'GOSPEL-5.png']
     },
@@ -257,7 +257,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'SK8 - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['SK8-1.png', 'SK8-2.png', 'SK8-3.png', 'SK8-4.png', 'SK8-5.png']
     },
@@ -267,7 +267,7 @@ export const NoteListRdzd: Notebook[] = [
         modelo: 'reduzido',
         genero: 'masculino',
         linha: 'HEROES - Red',
-        materias: ['10x1', '15X1', '20X1'],
+        materias: ['10 matérias', '15 matérias', '20 matérias'],
         valores: [434.40, 482.88, 449.82],
         url: ['HEROES-1.png', 'HEROES-2.png', 'HEROES-3.png', 'HEROES-4.png', 'HEROES-5.png']
     },
