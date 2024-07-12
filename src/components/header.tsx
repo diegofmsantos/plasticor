1ad97b9028f02875eb350faf3b424fc74e286cb1
diegofmsantos <diegofmsantos0@gmail.com>
2024-07-12 15:55:32 -0300

    ajustes header/login

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image"
 import { CartSidebar } from "./cart/sidebar"
 import { getServerSession } from 'next-auth'
-import { Button } from "./ui/button"
-import { signOut } from "next-auth/react"
 import LogOut from "./logout-button"
 import { redirect } from "next/navigation"
 
@@ -15,11 +13,11 @@ export const Header = async () => {
     return (
         <header
             className="fixed z-10 w-full flex justify-between h-32 items-center py-3 px-8 bg-[#002372]">
-            <div className="w-36 h-16">
+            <div className="w-30 h-12 md:w-36 md:h-16">
                 <Image src="/assets/logo.png" width={150} height={80} alt="Logo" className="w-full h-full" priority />
             </div>
-            <div className="flex items-center gap-5">
-                <p className="text-white">Olá, {session?.user?.name}</p>
+            <div className="flex flex-col items-center justify-center gap-1 md:flex-row md:gap-5">
+                <p className="text-white text-sm md:text-lg">Olá, {session?.user?.name}</p>
                 {session?.user?.image && (
                     <Image src={session.user.image} width={50} height={50} alt="User Image" className="rounded-full" />
                 )}
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button"
 export const LogOut = () => {
 
     return (
-        <Button className="bg-blue-500 h-7" onClick={() => signOut()}>Sair</Button>
+        <Button className="bg-blue-500 h-6 text-sm" onClick={() => signOut()}>Sair</Button>
     )
 }
 
