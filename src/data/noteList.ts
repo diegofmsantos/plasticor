b9f807fc5012861644f838d52c89e3e91d5e21e9
diegofmsantos <diegofmsantos0@gmail.com>
2024-07-30 12:31:44 -0300

    preços atualizados

--- src/data/noteList.ts
+++ src/data/noteList.ts
@@ -7,7 +7,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'THE GRAND THEFT',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['the-grand-theft-1.jpg', 'the-grand-theft-2.jpg', 'the-grand-theft-3.jpg', 'the-grand-theft-4.jpg', 'the-grand-theft-5.jpg']
     },
     {
@@ -16,7 +16,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'WARRIORS',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['warriors-1.jpg', 'warriors-2.jpg', 'warriors-3.jpg', 'warriors-4.jpg', 'warriors-5.jpg']
     },
     {
@@ -25,7 +25,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'MAD DOG',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['mad-dog-1.jpg', 'mad-dog-2.jpg', 'mad-dog-3.jpg', 'mad-dog-4.jpg', 'mad-dog-5.jpg']
     },
     {
@@ -34,7 +34,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'DAIJOUBU',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['daijoubu-1.jpg', 'daijoubu-2.jpg', 'daijoubu-3.jpg', 'daijoubu-4.jpg', 'daijoubu-5.jpg']
     },
     {
@@ -43,7 +43,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'FIGHT CLUB',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['fight-club-1.jpg', 'fight-club-2.jpg', 'fight-club-3.jpg', 'fight-club-4.jpg', 'fight-club-5.jpg']
     },
     {
@@ -52,7 +52,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'EPIC GAMES',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['epic-games-1.jpg', 'epic-games-2.jpg', 'epic-games-3.jpg', 'epic-games-4.jpg', 'epic-games-5.jpg']
     },
     {
@@ -61,7 +61,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'SK8',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['sk8-1.jpg', 'sk8-2.jpg', 'sk8-3.jpg', 'sk8-4.jpg', 'sk8-5.jpg']
     },
     {
@@ -70,7 +70,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'HEROES',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['heroes-1.jpg', 'heroes-2.jpg', 'heroes-3.jpg', 'heroes-4.jpg', 'heroes-5.jpg']
     },
     {
@@ -79,7 +79,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'MOTORCYCLES',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['motorcycles-1.jpg', 'motorcycles-2.jpg', 'motorcycles-3.jpg', 'motorcycles-4.jpg', 'motorcycles-5.jpg']
     },
     {
@@ -88,7 +88,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'SPORT MIX',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['sport mix-1.jpg', 'sport mix-2.jpg', 'sport mix-3.jpg', 'sport mix-4.jpg', 'sport mix-5.jpg']
     },
     {
@@ -97,7 +97,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'VELOZES E TUNADOS',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['velozes e tunados-1.jpg', 'velozes e tunados-2.jpg', 'velozes e tunados-3.jpg', 'velozes e tunados-4.jpg', 'velozes e tunados-5.jpg']
     },
     {
@@ -106,7 +106,7 @@ export const NoteList: Notebook[] = [
         modelo: 'masculino',
         linha: 'GOSPEL',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 646.50, 585.12, 694.08, 663.66],
+        valores: [545.76, 615.60, 557.28, 660.96, 631.98],
         url: ['gospel-1.jpg', 'gospel-2.jpg', 'gospel-3.jpg', 'gospel-4.jpg', 'gospel-5.jpg']
     },
     {
@@ -115,7 +115,7 @@ export const NoteList: Notebook[] = [
         linha: 'GOL COBRA',
         materias: ['15 matérias', '20 matérias'],
         modelo: 'masculino',
-        valores: [694.08, 663.66],
+        valores: [660.96, 631.98],
         url: ['gol-cobra-1.jpg', 'gol-cobra-1.jpg', 'gol-cobra-1.jpg', 'gol-cobra-1.jpg', 'gol-cobra-1.jpg']
     },
     {
@@ -124,7 +124,7 @@ export const NoteList: Notebook[] = [
         linha: 'GOL LEÃO',
         materias: ['15 matérias', '20 matérias'],
         modelo: 'masculino',
-        valores: [694.08, 663.66],
+        valores: [660.96, 631.98],
         url: ['gol-leao-1.jpg', 'gol-leao-1.jpg', 'gol-leao-1.jpg', 'gol-leao-1.jpg', 'gol-leao-1.jpg']
     },
     {
@@ -133,7 +133,7 @@ export const NoteList: Notebook[] = [
         linha: 'GOL URUBU',
         materias: ['15 matérias', '20 matérias'],
         modelo: 'masculino',
-        valores: [694.08, 663.66],
+        valores: [660.96, 631.98],
         url: ['gol-urubu-1.jpg', 'gol-urubu-2.jpg', 'gol-urubu-1.jpg', 'gol-urubu-2.jpg', 'gol-urubu-1.jpg']
     },
     {
@@ -142,7 +142,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'DOGS & CATS',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['dogs-e-cats-1.jpg', 'dogs-e-cats-2.jpg', 'dogs-e-cats-3.jpg', 'dogs-e-cats-4.jpg', 'dogs-e-cats-5.jpg']
     },
     {
@@ -151,7 +151,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'RAINBOW',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['rainbow-1.jpg', 'rainbow-2.jpg', 'rainbow-3.jpg', 'rainbow-4.jpg', 'rainbow-5.jpg']
     },
     {
@@ -160,7 +160,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'FULL ROSA',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['full-rosa-1.jpg', 'full-rosa-2.jpg', 'full-rosa-3.jpg', 'full-rosa-4.jpg', 'full-rosa-5.jpg']
     },
     {
@@ -169,7 +169,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'ALOHA',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['aloha-1.jpg', 'aloha-2.jpg', 'aloha-3.jpg', 'aloha-4.jpg', 'aloha-5.jpg']
     },
     {
@@ -178,7 +178,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'FLYS',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['flys-1.jpg', 'flys-2.jpg', 'flys-3.jpg', 'flys-4.jpg', 'flys-5.jpg']
     },
     {
@@ -187,7 +187,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'COSMUS',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['cosmus-1.jpg', 'cosmus-2.jpg', 'cosmus-3.jpg', 'cosmus-4.jpg', 'cosmus-5.jpg']
     },
     {
@@ -196,7 +196,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'COLOUR',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['colour-1.jpg', 'colour-2.jpg', 'colour-3.jpg', 'colour-4.jpg', 'colour-5.jpg']
     },
     {
@@ -205,7 +205,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'PETS',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['pets-1.jpg', 'pets-2.jpg', 'pets-3.jpg', 'pets-4.jpg', 'pets-5.jpg']
     },
     {
@@ -214,7 +214,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'CAPRICHE',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['capriche-1.jpg', 'capriche-2.jpg', 'capriche-3.jpg', 'capriche-4.jpg', 'capriche-5.jpg']
     },
     {
@@ -223,7 +223,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'UNICORN WORLD',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['unicorn world-1.jpg', 'unicorn world-2.jpg', 'unicorn world-3.jpg', 'unicorn world-4.jpg', 'unicorn world-5.jpg']
     },
     {
@@ -232,7 +232,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'LOVE',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['love-1.jpg', 'love-2.jpg', 'love-3.jpg', 'love-4.jpg', 'love-5.jpg']
     },
     {
@@ -241,7 +241,7 @@ export const NoteList: Notebook[] = [
         modelo: 'feminino',
         linha: 'CHARM',
         materias: ['1 matéria', '10 matérias', '12 matérias', '15 matérias', '20 matérias'],
-        valores: [716.40, 680.40, 615.84, 730.56, 699.84],
+        valores: [545.76, 648.00, 586.56, 695.76, 665.28],
         url: ['charm-1.jpg', 'charm-2.jpg', 'charm-3.jpg', 'charm-4.jpg', 'charm-5.jpg']
     },
     {
@@ -250,7 +250,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'TEEN - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [498.00, 559.44, 517.14],
+        valores: [452.70, 508.56, 470.16],
         url: ['teen-1.jpg', 'teen-2.jpg', 'teen-3.jpg', 'teen-4.jpg', 'teen-5.jpg']
     },
     {
@@ -259,7 +259,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'FLYS - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [498.00, 559.44, 517.14],
+        valores: [452.70, 508.56, 470.16],
         url: ['flys-1.jpg', 'flys-2.jpg', 'flys-3.jpg', 'flys-4.jpg', 'flys-5.jpg']
     },
     {
@@ -268,7 +268,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'CUTE - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [498.00, 559.44, 517.14],
+        valores: [452.70, 508.56, 470.16],
         url: ['cute-1.jpg', 'cute-2.jpg', 'cute-3.jpg', 'cute-4.jpg', 'cute-5.jpg']
     },
     {
@@ -277,7 +277,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'AMOUR - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [498.00, 559.44, 517.14],
+        valores: [452.70, 508.56, 470.16],
         url: ['amour-1.jpg', 'amour-2.jpg', 'amour-3.jpg', 'amour-4.jpg', 'amour-5.jpg']
     },
     {
@@ -286,7 +286,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'SPORTS - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [477.90, 531.12, 494.82],
+        valores: [434.40, 482.88, 449.82],
         url: ['sports-1.jpg', 'sports-2.jpg', 'sports-3.jpg', 'sports-4.jpg', 'sports-5.jpg']
     },
     {
@@ -295,7 +295,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'SHONEN - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [477.90, 531.12, 494.82],
+        valores: [434.40, 482.88, 449.82],
         url: ['shonen-1.jpg', 'shonen-2.jpg', 'shonen-3.jpg', 'shonen-4.jpg', 'shonen-5.jpg']
     },
     {
@@ -304,7 +304,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'GAMES - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [477.90, 531.12, 494.82],
+        valores: [434.40, 482.88, 449.82],
         url: ['games-1.jpg', 'games-2.jpg', 'games-3.jpg', 'games-4.jpg', 'games-5.jpg']
     },
     {
@@ -313,7 +313,7 @@ export const NoteList: Notebook[] = [
         modelo: 'basic',
         linha: 'CARS - BASIC',
         materias: ['10 matérias', '15 matérias', '20 matérias'],
-        valores: [477.90, 531.12, 494.82],
+        valores: [434.40, 482.88, 449.82],
         url: ['cars-1.jpg', 'cars-2.jpg', 'cars-3.jpg', 'cars-4.jpg', 'cars-5.jpg']
     },
     {
@@ -322,7 +322,7 @@ export const NoteList: Notebook[] = [
         modelo: 'infantil',
         linha: 'PRINCESS',
         materias: ['Espiralzinho', 'Brochurinha', 'Brochurão', 'Cartografia', 'Agenda'],
-        valores: [707.52, 809.58, 807.60, 712.80, 724.80],
+        valores: [675.84, 771.84, 771.00, 680.40, 691.80],
         url: ['princess-1.jpg', 'princess-2.jpg', 'princess-3.jpg', 'princess-4.jpg', 'princess-5.jpg']
     },
     {
@@ -331,7 +331,7 @@ export const NoteList: Notebook[] = [
         modelo: 'infantil',
         linha: 'UNICORN WORLD - INFANTIL',
         materias: ['Espiralzinho', 'Brochurinha', 'Brochurão', 'Cartografia', 'Agenda'],
-        valores: [707.52, 809.58, 807.60, 712.80, 724.80],
+        valores: [675.84, 771.84, 771.00, 680.40, 691.80],
         url: ['unicorn world-1.jpg', 'unicorn world-2.jpg', 'unicorn world-3.jpg', 'unicorn world-4.jpg', 'unicorn world-5.jpg']
     },
     {
@@ -340,7 +340,7 @@ export const NoteList: Notebook[] = [
         modelo: 'infantil',
         linha: 'KIDS',
         materias: ['Espiralzinho', 'Brochurinha', 'Brochurão', 'Cartografia', 'Agenda'],
-        valores: [707.52, 809.58, 807.60, 712.80, 724.80],
+        valores: [675.84, 771.84, 771.00, 680.40, 691.80],
         url: ['kids-1.jpg', 'kids-2.jpg', 'kids-3.jpg', 'kids-4.jpg', 'kids-5.jpg']
     },
     {
@@ -349,7 +349,7 @@ export const NoteList: Notebook[] = [
         modelo: 'infantil',
         linha: 'HEROES - INFANTIL',
         materias: ['Espiralzinho', 'Brochurinha', 'Brochurão', 'Cartografia', 'Agenda'],
-        valores: [707.52, 809.58, 807.60, 712.80, 724.80],
+        valores: [675.84, 771.84, 771.00, 680.40, 691.80],
         url: ['heroes-1.jpg', 'heroes-2.jpg', 'heroes-3.jpg', 'heroes-4.jpg', 'heroes-5.jpg']
     },
     {
@@ -358,7 +358,7 @@ export const NoteList: Notebook[] = [
         modelo: 'infantil',
         linha: 'PLAY',
         materias: ['Espiralzinho', 'Brochurinha', 'Brochurão', 'Cartografia', 'Agenda'],
-        valores: [707.52, 809.58, 807.60, 712.80, 724.80],
+        valores: [675.84, 771.84, 771.00, 680.40, 691.80],
         url: ['play-1.jpg', 'play-2.jpg', 'play-3.jpg', 'play-4.jpg', 'play-5.jpg']
     },
     {
